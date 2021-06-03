df9efaa37683d071c21a6418d276c325b7668fc5
flavioguisantos <flavio.guilherme@outlook.com.br>
2021-06-02 22:36:51 -0300

    05

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,37 +1,108 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// //
+// // This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// // convenience to get you started writing code faster.
+// //
+
+// export class BankAccount {
+//   constructor() {
+//     this.saldo = 0
+
+//   }
+
+//   open() {
+//     this.saldo
+//   }
+
+//   close() {
+    
+//     if(!this.saldo){
+//       throw new ValueError
+//     }else{
+//       this.accountOpen = false;
+//       this.accountBallance = 0;
+//     }
+//   }
+
+//   deposit(d) {
+
+//     this.saldo += d
+
+//   }
+
+//   withdraw(r) {
+//     this.saldo -= r
+//   }
+
+//   get balance() {
+//     return this.saldo
+//   }
+// }
+
+// export class ValueError extends Error {
+//   constructor() {
+//     super('Bank account error');
+//   }
+// }
+
 
 export class BankAccount {
+  
   constructor() {
-    this.saldo = 0
+    this.accountOpen = false;
+    this.accountBallance = 0;
   }
 
   open() {
-    return this.saldo
+    if(this.accountOpen){
+      throw new ValueError;
+    }
+    else{
+      this.accountOpen = true;
+      this.accountBallance = 0;
+    }
   }
 
   close() {
-
+    if(!this.accountOpen){
+      throw new ValueError;
+    }
+    else{
+      this.accountOpen = false;
+      this.accountBallance = 0;
+    }
   }
 
-  deposit(params) {
-    this.saldo == params 
-
+  deposit(value) {
+    if(value < 0 || !this.accountOpen){
+      throw new ValueError
+    }
+    else{
+      this.accountBallance += value;
+    }
   }
 
-  withdraw() {
-
+  withdraw(value) {
+    if(value > this.accountBallance || value < 0){
+      throw new ValueError
+    }
+    else{
+      this.accountBallance -= value;
+    }
   }
 
   get balance() {
-    return this.saldo
+    if(this.accountOpen){
+      return this.accountBallance
+    }
+    else{
+       throw new ValueError
+    }
   }
+
 }
 
 export class ValueError extends Error {
   constructor() {
     super('Bank account error');
-  }
+  } 
 }
