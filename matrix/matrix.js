4a60dd717ae7ff0c0055fb8a0d282e8ea5b36c80
flavioguisantos <flavio.guilherme@outlook.com.br>
2021-06-03 21:53:40 -0300

    06

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -3,25 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const Matrix = function (matrixString) {
-  this.matrix = [];
+export class Matrix {
+  constructor(str) {
+   this.matriz
+   this.result = []
+   this.str = str
 
-  matrixString.split("\n").forEach((rowString) => {
-    let rowList = [];
-    rowString
-      .split(" ")
-      .forEach((character) => rowList.push(parseInt(character)));
-    this.matrix.push(rowList);
-  });
-
-  this.rows = this.matrix;
-
-  this.columns = [];
-  for (let i = 0; i < this.matrix[0].length; i++) {
-    let columnList = [];
-    for (let j = 0; j < this.matrix.length; j++) {
-      columnList.push(this.matrix[j][i]);
-    }
-    this.columns.push(columnList);
   }
-};
+
+  get rows() {
+
+    
+   this.matriz = this.str.split('\n').map((p) => {
+    return p.replace(' ', '')
+  })
+
+  for (var i = 0; i < this.matriz.length; i++) {
+    this.result.push([...this.matriz[i]])
+  }
+
+  }
+}
